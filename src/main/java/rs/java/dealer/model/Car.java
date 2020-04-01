63b1ba72271415d776a5613e0081673be71f9388
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-01 18:02:14 +0200

    Car clas finished

--- src/main/java/rs/java/dealer/model/Car.java
+++ src/main/java/rs/java/dealer/model/Car.java
@@ -1,7 +1,89 @@
 package rs.java.dealer.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
+@Table(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_id")
+    private int id;
+    private String brand;
+    private String model;
+    private String color;
+    private double millage;
+    private BigDecimal price;
+    private Boolean isNew;
+
+    public Car() {
+    }
+
+    public Car(String brand, String model, String color,
+               double millage, BigDecimal price, Boolean isNew) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.millage = millage;
+        this.price = price;
+        this.isNew = isNew;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMillage() {
+        return millage;
+    }
+
+    public void setMillage(double millage) {
+        this.millage = millage;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
 }
